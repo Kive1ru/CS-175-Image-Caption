407cc99ad56df7fc55edd337fef4ab36d28fa5e9
Kylin Guo <kylinguo627@outlook.com>
2022-11-13 16:38:07 -0800

    dataset update

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from torch.utils.data import DataLoader,Dataset
 import tensorflow as tf
+from PIL import Image
+import torchvision.transforms as transforms
 
 class FDataset(Dataset):
     
@@ -20,13 +22,25 @@ class FDataset(Dataset):
     
     def __getitem__(self,idx):
         caption = self.captions[idx]
-        img_name = "Images/" + self.imgs[idx]
+        img_name = self.imgs[idx]
         img_location = os.path.join(self.root_dir,img_name)
-        img = tf.io.read_file(img_location)
+        img = Image.open(img_location)
         if self.transform is not None:
             img = self.transform(img)
         caption = caption.split()
         caption = [word.lower() for word in caption]
         caption = "startseq " + ' '.join(caption) + " endseq"
         return img, self.tokenizer.texts_to_sequences([caption])[0]
-    
+
+
+'''if __name__ == "__main__":
+    BASE_DIR = f"{os.getcwd()}/data/flickr8k"
+    transformer = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor()])
+    dataset =  FDataset(
+        root_dir = BASE_DIR+"/Images",
+        capFilename = BASE_DIR+"/captions.txt",
+        transform=transformer
+    )
+    print(dataset[0])'''
